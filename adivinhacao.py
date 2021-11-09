aa66d400de6afc5f66515fbc04b814d1c4308478
Claudio <claudiosfcarvalho@outlook.com>
2021-11-09 08:11:33 -0300

    criacao de jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,2 +1,20 @@
+print("*********************************")
 print("Bem vind@ no jogo de Adivinhação!")
 print("*********************************")
+
+numero_secreto = 42
+
+chute = input("Digite o seu número: ")
+
+print("Você digitou", chute)
+
+if(chute.isnumeric()):
+    numero_chute = int(chute)
+    if(numero_secreto == numero_chute):
+        print("Acertou")
+    else:
+        print("Errou")
+else:
+    print("Somente números são aceitos")
+
+print("Fim do Jogo")
