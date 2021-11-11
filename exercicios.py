0e63ef29482aa4104407167c278b8541a03c4ed0
Claudio <claudiosfcarvalho@outlook.com>
2021-11-10 22:26:48 -0300

    aula 5

---
+++ exercicios.py
@@ -0,0 +1,22 @@
+#Aula 5 - Praticando format
+print("Tentativa {} de {}".format(3,10))
+
+print("Tentativa {1} de {0}".format(3,10))
+
+print("R$ {}".format(1.59))
+
+print("R$ {:f}".format(1.59))
+
+print("R$ {:.2f}".format(1.59))
+
+print("R$ {:7.2f}".format(1234.59))
+
+print("R$ {:7.2f}".format(4.59))
+
+print("R$ {:07.2f}".format(4.59))
+
+print("R$ {:07d}".format(4))
+
+print("R$ {:7d}".format(4))
+
+print("Data {:2d}/{:2d}".format(19, 11))
