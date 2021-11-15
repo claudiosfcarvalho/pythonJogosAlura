209246aaa6423c59bf93e5bd0caa5cf67d925596
Claudio <claudiosfcarvalho@outlook.com>
2021-11-15 19:21:36 -0300

    parte 2 - aula 5

--- forca.py
+++ forca.py
@@ -4,9 +4,11 @@ def jogar():
     print("Bem vind@ no jogo de Forca!")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     enforcou = False
     acertou = False
+    tentativas = 0
+    maximo_tentativas = 6
     letras_acertadas = []
     pos = 0
 
@@ -17,18 +19,29 @@ def jogar():
     print(letras_acertadas)
 
     while(not enforcou and not acertou):
+
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
         index = 0
 
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            tentativas += 1
+            print(f"Erro {tentativas} de {maximo_tentativas}")
 
-        if(letras_acertadas.count("_") == 0):
-            acertou = True
+        acertou = letras_acertadas.count("_") == 0
+        enforcou = tentativas == maximo_tentativas
+
         print(letras_acertadas)
+
+    if(acertou):
+        print("Você acertou")
+    else:
+        print("Você errou")
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
