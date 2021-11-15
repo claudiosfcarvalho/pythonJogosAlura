485781bc7db1234000cb29b903045a3c645b25f5
Claudio <claudiosfcarvalho@outlook.com>
2021-11-15 17:38:24 -0300

    parte 2 - aula 3

--- forca.py
+++ forca.py
@@ -7,15 +7,28 @@ def jogar():
     palavra_secreta = "banana"
     enforcou = False
     acertou = False
+    letras_acertadas = []
+    pos = 0
+
+    while(pos < len(palavra_secreta)):
+        letras_acertadas.append("_")
+        pos = pos + 1
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
         chute = input("Qual letra? ")
         chute = chute.strip()
         index = 0
+
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}", format(chute, index))
-        print("jogando")
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        if(letras_acertadas.count("_") == 0):
+            acertou = True
+        print(letras_acertadas)
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
