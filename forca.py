601d99ec69395796f018f88990d192a077e2637a
Claudio <claudiosfcarvalho@outlook.com>
2021-11-15 15:25:28 -0300

    Parte 2 - Aula 1

--- forca.py
+++ forca.py
@@ -4,6 +4,12 @@ def jogar():
     print("Bem vind@ no jogo de Forca!")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
